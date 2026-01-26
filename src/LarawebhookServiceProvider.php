7eb4e7837dcdfdc1c801fe09eb27811d168f0bfe
Proxynth <proxynth.tech@gmail.com>
2026-01-26 18:37:36 +0100

    feat: :sparkles: add configuration for webhook services and update service provider

--- src/LarawebhookServiceProvider.php
+++ src/LarawebhookServiceProvider.php
@@ -2,12 +2,31 @@
 
 namespace Proxynth\Larawebhook;
 
+use Illuminate\Foundation\Console\AboutCommand;
 use Proxynth\Larawebhook\Commands\SkeletonCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 class LarawebhookServiceProvider extends PackageServiceProvider
 {
+    public function boot(): void
+    {
+        parent::boot();
+
+        $this->publishes([
+            __DIR__.'/../config/larawebhook.php' => config_path('larawebhook.php'),
+        ]);
+
+        $this->publishesMigrations([
+            __DIR__.'/../database/migrations' => database_path('migrations'),
+        ]);
+
+        AboutCommand::add('Larawebhook', fn () => [
+            'name' => 'Larawebhook',
+            'Version' => '0.0.0',
+        ]);
+    }
+
     public function configurePackage(Package $package): void
     {
         $package->name('larawebhook')
