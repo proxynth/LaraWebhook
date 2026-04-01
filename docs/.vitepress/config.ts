b2f193cc5f9e33ee5e688a74caa7213bf794efb2
Proxynth <proxynth.tech@gmail.com>
2026-04-01 17:07:45 +0200

    docs: :memo: change date in docs

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -68,7 +68,7 @@ export default defineConfig({
 
     footer: {
       message: 'Released under the MIT License.',
-      copyright: 'Copyright © 2024 Proxynth',
+      copyright: 'Copyright © 2026 Proxynth',
     },
 
     search: {
